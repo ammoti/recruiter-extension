075669fb3832629c050236c11d898142e984307f
ammoti <vahap.yigit@gmail.com>
2020-05-13 13:29:26 +0300

    Added functionatlity and code refactor

--- app/scripts/contentscript.ts
+++ app/scripts/contentscript.ts
@@ -17,71 +17,129 @@ chrome.runtime.onMessage.addListener((msg, sender, response) => {
     // Collect the necessary data.
     // (For your specific requirements `document.querySelectorAll(...)`
     //  should be equivalent to jquery's `$(...)`.)
-    const name = document.getElementsByClassName(
-      "inline t-24 t-black t-normal break-words"
-    )[0].innerHTML;
-    const title = document.getElementsByClassName(
-      "mt1 t-18 t-black t-normal break-words"
-    )[0].innerHTML;
-    const location = document.getElementsByClassName(
-      "t-16 t-black t-normal inline-block"
-    )[0].innerHTML;
-    // let about = document.getElementsByClassName(
-    //   "pv-about__summary-text mt4 t-14 ember-view"
-    // )[0] as HTMLElement;
+    let name = checkInfoExist(
+      document.getElementsByClassName(
+        "inline t-24 t-black t-normal break-words"
+      )[0],
+      true
+    );
+    let title = checkInfoExist(
+      document.getElementsByClassName(
+        "mt1 t-18 t-black t-normal break-words"
+      )[0],
+      true
+    );
+    let location = checkInfoExist(
+      document.getElementsByClassName("t-16 t-black t-normal inline-block")[0],
+      true
+    );
+    let about =
+      document.getElementsByClassName(
+        "pv-about__summary-text mt4 t-14 ember-view"
+      ).length === 0
+        ? "Not information"
+        : checkInfoExist(
+            document.getElementsByClassName(
+              "pv-about__summary-text mt4 t-14 ember-view"
+            )[0],
+            false
+          );
+
     // TODO: burası düzeltilecek innerHTML veya innerText lerin kontrolü şart func a bağlanabilinir.
-    // if (!about) about = new HTMLElement();
-    const imgUrl = document
-      .getElementsByClassName(
-        "pv-top-card__photo presence-entity__image EntityPhoto-circle-9 lazy-image ember-view"
-      )[0]
-      .getAttribute("src");
-    const experience = document
+    // let imgUrl = document
+    //   .getElementsByClassName(
+    //     "pv-top-card__photo presence-entity__image EntityPhoto-circle-9 lazy-image ember-view"
+    //   )[0]
+    //   .getAttribute("src");
+    // console.log("imgurl", imgUrl);
+    // if (imgUrl) imgUrl = "";
+    let experience = document
       .getElementsByClassName(
         "pv-profile-section__section-info section-info"
       )[0]
-      .getElementsByTagName("li");
+      .getElementsByClassName(
+        "pv-entity__position-group-pager pv-profile-section__list-item ember-view"
+      );
+    // if(!experience) experience = "" as HTMLCollectionOf<HTMLElement>();
     const skills = document.getElementsByClassName(
       "pv-skill-category-entity__name-text t-16 t-black t-bold"
     );
- 
+    const expInfo = processExp(experience);
     const domInfo = {
-      name: name,
-      title: title,
-      location: location,
-      imgUrl: imgUrl,
-      // about: about.innerText,
-      experiences: processExp(experience),
+      name: name?.trim(),
+      title: title?.trim(),
+      location: location?.trim(),
+      about: about?.trim(),
+      experiences: expInfo,
       skills: processSkill(skills),
     };
-
+    console.log(domInfo);
     // Directly respond to the sender (popup),
     // through the specified callback.
     response(domInfo);
   }
 });
 
-function processExp(exp: HTMLCollectionOf<HTMLElement>) {
+function processExp(exp: HTMLCollectionOf<Element>) {
   let experiences: string[] = [];
   for (const key in exp) {
     if (exp.hasOwnProperty(key)) {
       const element = exp[key];
-      const position = element.getElementsByClassName("t-16 t-black t-bold")[0]
-        .innerHTML;
-      const company = element.getElementsByClassName(
-        "pv-entity__secondary-title t-14 t-black t-normal"
-      )[0].innerHTML;
-      const date = element.getElementsByClassName(
-        "pv-entity__date-range t-14 t-black--light t-normal"
-      )[0].innerHTML;
-      const experienceDetail: any = {};
-      if (position) experienceDetail.position = position;
-      if (company) experienceDetail.company = company;
-      if (date) experienceDetail.date = date;
-      experiences.push(experienceDetail);
+      if (
+        element.getElementsByClassName("pv-entity__position-group mt2").length >
+        0
+      ) {
+        const company = checkInfoExist(
+          element.getElementsByClassName("t-16 t-black t-bold")[0],
+          false
+        );
+        const experienceDetail: any = {};
+        if (company) {
+          experienceDetail.company = { name: company, item: {} };
+          const subPosition = element.getElementsByTagName("li");
+          for (const subKey in subPosition) {
+            const positionDetail: any = {};
+            if (subPosition.hasOwnProperty(subKey)) {
+              const title = subPosition[subKey]
+                .getElementsByTagName("h3")[0]
+                .innerText.replace("Title", "")
+                .trim();
+              if (title) positionDetail.title = title;
+              const duration = subPosition[subKey]
+                .getElementsByTagName("h4")[0]
+                .innerText.replace("Dates Employed", "")
+                .trim();
+              if (duration) positionDetail.duration = duration;
+              experienceDetail.company.item[subKey] = positionDetail;
+            }
+          }
+        }
+        experiences.push(experienceDetail);
+      } else {
+        const position = checkInfoExist(
+          element.getElementsByClassName("t-16 t-black t-bold")[0],
+          true
+        );
+        const company = checkInfoExist(
+          element.getElementsByClassName(
+            "pv-entity__secondary-title t-14 t-black t-normal"
+          )[0],
+          true
+        );
+        const date = checkInfoExist(
+          element.getElementsByClassName(
+            "pv-entity__date-range t-14 t-black--light t-normal"
+          )[0],
+          true
+        );
+        const experienceDetail: any = {};
+        if (company) experienceDetail.company = { name: company };
+        if (position) experienceDetail.company.position = position;
+        if (date) experienceDetail.company.date = date;
+        experiences.push(experienceDetail);
+      }
     }
   }
-  console.log("experiences",experiences);
   return experiences;
 }
 function processSkill(exp: HTMLCollectionOf<Element>) {
@@ -89,10 +147,9 @@ function processSkill(exp: HTMLCollectionOf<Element>) {
   for (const key in exp) {
     if (exp.hasOwnProperty(key)) {
       const element = exp[key];
-      skills.push(element.innerHTML);
+      skills.push(checkInfoExist(element, true)!.trim());
     }
   }
-  console.log("skill",skills);
   return skills;
 }
 let isExpanded = false;
@@ -108,11 +165,11 @@ function whellActivated() {
       if (moreExperiences) {
         moreExperiences.click();
       }
+      document.getElementById("line-clamp-show-more-button")?.click();
       const skills = document.getElementsByClassName(
         "pv-profile-section__card-action-bar pv-skills-section__additional-skills"
       )[0] as HTMLElement;
       if (skills) {
-        console.log(skills.innerText);
         if (skills.innerText.toLocaleLowerCase().search("more") !== -1)
           skills.click();
       }
@@ -128,4 +185,9 @@ function whellActivated() {
 }
 function log() {
   console.log("gitti");
+}
+function checkInfoExist(htmlParam: Element, isHTML: boolean) {
+  if (htmlParam === undefined) return "Not Information";
+  if (isHTML) return htmlParam.innerHTML;
+  return htmlParam.textContent;
 }
--- app/scripts/popup.ts
+++ app/scripts/popup.ts
@@ -11,12 +11,13 @@ console.log(`'Allo 'Allo! Popup`);
 //       port.postMessage({ question: "Who's there?" });
 //     else if (msg.answer === "Madame")
 //       port.postMessage({ question: "Madame who?" });
-//     else if (msg.answer === "Madame... Bovary")
+//     else if (msg.answer === "Madame... Boconsty")
 //       port.postMessage({ question: "I don't get it." });
 //   });
 // });
-
+let profileInfo: any;
 const setDOMInfo = (info: any) => {
+  profileInfo = info;
   const name = document.getElementById("name");
   const title = document.getElementById("title");
   const location = document.getElementById("location");
@@ -35,14 +36,69 @@ const setDOMInfo = (info: any) => {
     for (let i = 0; i < info.experiences.length; i++) {
       const element = info.experiences[i];
       const item: Node = document.createElement("li");
-      item.textContent = element.company.replace("<!---->", "");
-      experiences.appendChild(item);
+      if (element.company.hasOwnProperty("date")) {
+        item.textContent =
+          element.company.name
+            .replace("<!---->", "")
+            // tslint:disable-next-line: quotemark
+            .replace('<span class="pv-entity__secondary-title separator">', "")
+            .replace("</span", "") +
+          " - " +
+          element.company.position;
+        experiences.appendChild(item);
+      } else {
+        let position = "";
+        if (element.company.hasOwnProperty("item")) {
+          for (let i = 0; i < Object.keys(element.company.item).length; i++) {
+            const subItem = element.company.item[i];
+            position += subItem.title + " ";
+          }
+          item.textContent =
+            element.company.name
+              .replace("Company Name", "")
+              .replace("<!---->", "")
+              .replace(
+                // tslint:disable-next-line: quotemark
+                '<span class="pv-entity__secondary-title separator">',
+                ""
+              )
+              .replace("</span>", "")
+              .trim() +
+            " - " +
+            position;
+          experiences.appendChild(item);
+        } else {
+          item.textContent =
+            element.company.name
+              .replace("<!---->", "")
+              .replace(
+                // tslint:disable-next-line: quotemark
+                '<span class="pv-entity__secondary-title separator">',
+                ""
+              )
+              .replace("</span>", "")
+              .trim() + " - ";
+          experiences.appendChild(item);
+        }
+      }
     }
   }
 };
-
 // Once the DOM is ready...
 window.addEventListener("DOMContentLoaded", () => {
+  const link = document.getElementById("exportCSV");
+  // onClick's logic below:
+  if (link) {
+    link.addEventListener("click", function () {
+      exportCSV();
+    });
+  }
+  const addProfile = document.getElementById("addProfile");
+  if (addProfile) {
+    addProfile.addEventListener("click", function () {
+      addLocal(profileInfo);
+    });
+  }
   // ...query for the active tab...
   console.log("loaded");
   chrome.tabs.query(
@@ -62,3 +118,168 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   );
 });
+function exportCSV() {
+  const downloadData = local.get("profileList");
+  JSONToCSVConvertor([downloadData], "LinkedIn Profiles", "Profiles");
+}
+
+function JSONToCSVConvertor(JSONData: any, ReportTitle: any, ShowLabel: any) {
+  console.log("JSONDATA", JSONData);
+  const array = typeof JSONData !== "object" ? JSON.parse(JSONData) : JSONData;
+  let str = "";
+  let line = "";
+  let skillsCount = 1;
+  let experienceCount = 1;
+  for (const key in array[0]) {
+    line = "";
+    experienceCount = 1;
+    skillsCount = 1;
+    for (const item in array[0][key]) {
+      if (array[0][key].hasOwnProperty(item)) {
+        const value = array[0][key][item];
+        switch (item) {
+          case "name":
+            if (array[key] !== "") {
+              let text: string = value;
+              line += text
+                .trim()
+                .replace(/(\r\n|\n|\r)/gm, " ")
+                .replace(/,/g, "");
+            }
+            break;
+          case "about":
+            if (array[key] !== "") {
+              let text: string = value;
+              line += text
+                .trim()
+                .replace(/(\r\n|\n|\r)/gm, " ")
+                .replace(/,/g, "");
+            }
+            break;
+          case "location":
+            if (array[key] !== "") {
+              let text: string = value;
+              line += text
+                .trim()
+                .replace(/(\r\n|\n|\r)/gm, " ")
+                .replace(/,/g, "");
+            }
+            break;
+          case "title":
+            if (array[key] !== "") {
+              let text: string = value;
+              line += text
+                .trim()
+                .replace(/(\r\n|\n|\r)/gm, " ")
+                .replace(/,/g, "");
+            }
+            break;
+          case "experiences":
+            for (let exp = 0; exp < value.length; exp++) {
+              const element = value[exp];
+              if (element)
+                line +=
+                  element.company.name
+                    .trim()
+                    .replace(/(\r\n|\n|\r)/gm, " ")
+                    .replace("Company Name", "")
+                    .replace(/,/g, "")
+                    .replace("<!---->", "") + "\n";
+              line += ",,,,";
+            }
+            experienceCount = value.length;
+            break;
+          case "skills":
+            for (let exp = 0; exp < value.length; exp++) {
+              const element = value[exp];
+              if (element) {
+                line +=
+                  element
+                    .trim()
+                    .replace(/(\r\n|\n|\r)/gm, " ")
+                    .replace("Company Name", "")
+                    .replace(/,/g, "")
+                    .replace("<!---->", "") + "\n";
+              }
+              line += ",,,,,";
+              skillsCount = value.length;
+            }
+            break;
+          default:
+            break;
+        }
+        line += ",";
+      }
+    }
+    line.slice(0, line.length - 1);
+    if (experienceCount < skillsCount) experienceCount = skillsCount;
+    for (let b = 0; b < experienceCount; b++) {
+      line += "\r\n";
+    }
+    str += line;
+    line = "";
+  }
+  console.log("final", str);
+  // Generate a file name
+  let fileName = "MyReport_";
+  // this will remove the blank-spaces from the title and replace it with an underscore
+  fileName += ReportTitle.replace(/ /g, "_");
+
+  // Initialize file format you want csv or xls
+  const uri = "data:text/csv;charset=UTF-8," + escape(str);
+
+  // Now the little tricky part.
+  // you can use either>> window.open(uri);
+  // but this will not work in some browsers
+  // or you will not get the correct file extension
+
+  // this trick will generate a temp <a /> tag
+  let link = document.createElement("a");
+  link.href = uri;
+
+  // set the visibility hidden so it will not effect on your web-layout
+  // link.style = "visibility:hidden";
+  link.download = fileName + ".csv";
+
+  // this part will append the anchor tag and remove it after automatic click
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+function addLocal(info: any) {
+  console.log("get item", local.get("profileList"));
+  let profileList: string[] = local.get("profileList");
+  profileList.push(info);
+  local.set("profileList", profileList);
+  console.log(profileList);
+}
+const local = (function () {
+  const setData = function (key: any, obj: any) {
+    const values = JSON.stringify(obj);
+    localStorage.setItem(key, values);
+  };
+
+  const getData = function (key: any) {
+    if (localStorage.getItem(key) != null) {
+      return JSON.parse(localStorage.getItem(key) || "{}");
+    } else {
+      return false;
+    }
+  };
+
+  const updateDate = function (key: any, newData: any) {
+    if (localStorage.getItem(key) != null) {
+      const oldData = JSON.parse(localStorage.getItem(key) || "{}");
+      for (const keyObj in newData) {
+        oldData[keyObj] = newData[keyObj];
+      }
+      const values = JSON.stringify(oldData);
+      localStorage.setItem(key, values);
+    } else {
+      return false;
+    }
+    return false;
+  };
+
+  return { set: setData, get: getData, update: updateDate };
+})();
